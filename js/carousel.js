22206ad34a952851084398603e11508200b1a0a7
LuciaCasim <lucia.casim@gmail.com>
2023-03-07 12:03:50 -0300

    link-change

--- js/carousel.js
+++ js/carousel.js
@@ -1,53 +1,53 @@
 
 
-// Set all carousel items to the same height
+// // Set all carousel items to the same height
 
 
-function carouselNormalization(carousel) {
+// function carouselNormalization(carousel) {
         
-    window.heights = [], //create empty array to store height values
-    window.tallest; //create variable to make note of the tallest slide
+//     window.heights = [], //create empty array to store height values
+//     window.tallest; //create variable to make note of the tallest slide
     
-    function normalizeHeights(param1) {
-        jQuery(`${param1} .carousel-inner .carousel-item`).each(function() { //add heights to array
-            window.heights.push(jQuery(this).outerHeight());
-        });
+//     function normalizeHeights(param1) {
+//         jQuery(`${param1} .carousel-inner .carousel-item`).each(function() { //add heights to array
+//             window.heights.push(jQuery(this).outerHeight());
+//         });
 
-        window.tallest = Math.max.apply(null, window.heights); //cache largest value
-        jQuery(`${param1} .carousel-inner .carousel-item`).each(function() {
-            jQuery(this).css('min-height',tallest + 'px');
-        });     
+//         window.tallest = Math.max.apply(null, window.heights); //cache largest value
+//         jQuery(`${param1} .carousel-inner .carousel-item`).each(function() {
+//             jQuery(this).css('min-height',tallest + 'px');
+//         });     
             
-        }
+//         }
           
     
 
-    normalizeHeights(`${carousel}`);
+//     normalizeHeights(`${carousel}`);
     
 
-    jQuery(window).on('resize orientationchange', function () {
+//     jQuery(window).on('resize orientationchange', function () {
         
-        window.tallest = 0, window.heights.length = 0; //reset vars
-        jQuery(`${carousel} .carousel-inner .carousel-item`).each(function() {
-            jQuery(this).css('min-height','0'); //reset min-height
-        }); 
+//         window.tallest = 0, window.heights.length = 0; //reset vars
+//         jQuery(`${carousel} .carousel-inner .carousel-item`).each(function() {
+//             jQuery(this).css('min-height','0'); //reset min-height
+//         }); 
         
-        normalizeHeights(`${carousel}`); //run it again 
+//         normalizeHeights(`${carousel}`); //run it again 
 
 
-    });
+//     });
 
 
     
-}
+// }
 
 
-jQuery( document ).ready(function() {
-    carouselNormalization(".parabWhy");
-    carouselNormalization(".parabReco");
+// jQuery( document ).ready(function() {
+//     carouselNormalization(".parabWhy");
+//     carouselNormalization(".parabReco");
 
-    $( window ).resize(function() {
-        carouselNormalization(".parabWhy");
-        carouselNormalization(".parabReco");
-      });
-});
+//     $( window ).resize(function() {
+//         carouselNormalization(".parabWhy");
+//         carouselNormalization(".parabReco");
+//       });
+// });
